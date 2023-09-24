76d4c6225452e4fe6b1d209c821bacd754ff4c1b
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-09-24 10:35:43 +0900

    take known config

--- src/PixivFanbox/Front/Brick.hs
+++ src/PixivFanbox/Front/Brick.hs
@@ -7,16 +7,23 @@ import Brick.BChan (newBChan)
 import qualified Brick.Focus as F
 import qualified Brick.Main as M
 import qualified Brick.Types as T
+import Brick.Util (style)
 import qualified Brick.Widgets.Center as C
-import Brick.Widgets.Core (str)
+import Brick.Widgets.Core (hLimit, modifyDefAttr, str, (<+>))
 import qualified Brick.Widgets.Edit as E
+import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Graphics.Vty as V
 import Lens.Micro ((^?!))
+import Lens.Micro.Mtl (zoom)
 import Lens.Micro.TH (makeLenses)
 import PixivFanbox.Api.Entity (SupportingCreator)
-import PixivFanbox.Config (Config)
+import PixivFanbox.Config (Config (configSessionId))
 
-data SettingForm = SessionIdInputField deriving (Show, Eq, Ord)
+data SettingForm
+  = SessionIdInputField
+  | SessionIdModificationSave
+  | SessionIdModificationCancel
+  deriving (Show, Eq, Ord)
 
 data SupportingCreatorForm
   = SupportingCreatorListField
@@ -51,7 +58,7 @@ data State
       }
   | Configuring
       { _configureringFocusRing :: F.FocusRing ResourceName,
-        _session :: E.Editor String ResourceName,
+        _sessionEdit :: E.Editor String ResourceName,
         _currentConfig :: Maybe Config
       }
   deriving (Show)
@@ -59,13 +66,7 @@ data State
 makeLenses ''State
 
 initialState :: Maybe Config -> State
-initialState Nothing =
-  Configuring
-    ( F.focusRing [Setting SessionIdInputField]
-    )
-    ( E.editor (Setting SessionIdInputField) (Just 1) ""
-    )
-    Nothing
+initialState Nothing = startConfiguring Nothing
 initialState (Just c) =
   Explorering
     c
@@ -77,29 +78,55 @@ initialState (Just c) =
     Nothing
     []
 
+startConfiguring :: Maybe Config -> State
+startConfiguring c =
+  Configuring
+    ( F.focusRing [Setting SessionIdInputField]
+    )
+    ( E.editor (Setting SessionIdInputField) (Just 1) (maybe "" (B.unpack . configSessionId) c)
+    )
+    c
+
 drawUI :: State -> [T.Widget ResourceName]
 drawUI s@(Configuring {}) = [ui]
   where
-    c = F.withFocusRing (s ^?! configureringFocusRing) (E.renderEditor (str . unlines)) (s ^?! session)
-    ui = C.center $ str "session id: "
+    e =
+      F.withFocusRing
+        (s ^?! configureringFocusRing)
+        (E.renderEditor (str . unlines))
+        (s ^?! sessionEdit)
+    ui = C.center $ str "session id: " <+> modifyDefAttr (const $ style V.underline) (hLimit 50 e)
 drawUI (Explorering {}) = [ui]
   where
     ui = str "logged in"
 
+handleConfiguring :: State -> T.BrickEvent ResourceName Event -> T.EventM ResourceName State ()
+handleConfiguring (Explorering {}) _ = error "invalid handler"
+handleConfiguring _s@(Configuring {}) ev = do
+  zoom sessionEdit $ E.handleEditorEvent ev
+
+handleExploering :: State -> T.BrickEvent ResourceName Event -> T.EventM ResourceName State ()
+handleExploering (Configuring {}) _ = error "invalid handler"
+handleExploering s@(Explorering {}) ev = do
+  case ev of
+    (T.VtyEvent (V.EvKey (V.KChar 's') [])) -> do
+      T.put (startConfiguring (Just $ s ^?! config))
+      return ()
+    _ -> return ()
+
 appEvent :: T.BrickEvent ResourceName Event -> T.EventM ResourceName State ()
 appEvent (T.VtyEvent (V.EvKey V.KEsc [])) = M.halt
-appEvent (T.VtyEvent (V.EvKey (V.KChar 'q') [])) = M.halt
-appEvent (T.VtyEvent (V.EvKey (V.KChar 's') [])) = do
-  currentState <- T.get
-  case currentState of
-    Configuring {} -> return ()
-    Explorering {} -> do
-      T.put (initialState Nothing)
-      return ()
-appEvent _ = return ()
+appEvent ev = do
+  s <- T.get
+  case s of
+    Configuring {} -> handleConfiguring s ev
+    Explorering {} -> handleExploering s ev
 
 appMap :: A.AttrMap
-appMap = A.attrMap V.defAttr []
+appMap =
+  A.attrMap
+    V.defAttr
+    []
 
 appCursor :: State -> [T.CursorLocation ResourceName] -> Maybe (T.CursorLocation ResourceName)
 appCursor = M.showFirstCursor
